975478decdf9b9d4bf3a7940eec58ac29eedd721
Henrique Breim <breim@protonmail.ch>
2015-12-07 17:46:31 -0200

    Add admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   	get 'myproducts', on: :collection, path: 'meus-produtos'
   end
   resources :comments, only: [:create, :destroy, :index], path: :mensagens
+
+  # Admin routes
+  get '/admin' => "admin/admin#index", as: :admin 
+  namespace :admin do
+  	resources :products
+  end
 end
