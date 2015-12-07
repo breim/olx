87947b56c20c6eff6de7eabae66d2ad80de029a2
Henrique Breim <breim@protonmail.ch>
2015-12-07 17:42:00 -0200

    Add my products route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, path: :anuncios
+  resources :products, path: :anuncios do
+  	get 'myproducts', on: :collection, path: 'meus-produtos'
+  end
   resources :comments, only: [:create, :destroy]
 end
